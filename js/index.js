7429d89c7bc9634e2ec614c3a70b0d91a0dfb260
keitaaki <sj860069@gmail.com>
2020-12-31 16:15:28 +0900

    2020/12/30⑤

--- js/index.js
+++ js/index.js
@@ -8,16 +8,81 @@ var swiper = new Swiper('.swiper-container', {
   slidesPerView: 2,
 });
 
-$(function() {
+// ハンバーガーからバツへ
+$(function () {
   $('.navbar_toggle').on('click', function () {
-   $('.menu-trigger').toggleClass('active');
-   $('#sp_global_nav').toggleClass('hidden');
-   $('#sp_contact_box').toggleClass('hidden');
+    $('.menu-trigger').toggleClass('active');
+    $('#sp_global_nav').toggleClass('hidden');
+    $('#sp_contact_box').toggleClass('hidden');
   });
- });
+});
 
-$(function() {
-  $('#sp_sub_menus').on('click', function () {
-   $('.sub_menu').toggleClass('hidden');
+// スクロールするとついてくるハンバーガーメニュ
+$(function () {
+  $('.navbar_toggle_open').on('click', function () {
+    $('.menu-trigger_open').toggleClass('active');
   });
- });
+});
+
+// ヘッダーの中のサブメニュー（SP）
+$(function () {
+  $('#sp_sub_menus').on('click', function () {
+    $('.sub_menu').toggleClass('hidden');
+  });
+});
+
+// ヘッダーの中のサブメニュー（PC）
+$(function () {
+  $('#pc_global_menus').on('click', function () {
+    $('.sub_menu').toggleClass('hidden');
+  });
+});
+
+
+//  スクロールしたらついてくるメニューの実装
+$(function () {
+  var topBtn = $('.contact_box_humber');
+  //ボタンを非表示にする
+  topBtn.hide();
+  //スクロールしてページトップから100に達したらボタンを表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      //フェードインで表示
+      topBtn.fadeIn();
+    } else {
+      //フェードアウトで非表示
+      topBtn.fadeOut(1);
+    }
+  });
+});
+
+
+//  スクロールしたらついてくるメニューの実装
+$(function () {
+  var topBtn = $('.scroll_menu_open');
+  var hamburgerBtn = $('.menu-trigger_open');
+  //ボタンを非表示にする
+  topBtn.hide();
+  //スクロールしてページトップから100に達したらボタンを表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      //フェードインで表示
+      
+    } else {
+      //フェードアウトで非表示
+      topBtn.fadeOut(1);
+      hamburgerBtn.removeClass("active");
+    }
+  });
+
+  $('#scroll_hamburger').on('click', function () {
+    topBtn.fadeToggle();
+  });
+});
+
+//  スクロールしたらついてくるメニューの実装の中のサブメニュー
+$(function () {
+  $('#global_menus_open').on('click', function () {
+    $('.sub_menu_scroll').toggleClass('hidden');
+  });
+});
