8430b303f6e5ffed3679f67c96ca55e65362c22b
keitaaki <sj860069@gmail.com>
2021-01-26 06:26:57 +0900

    2021/01/26

--- common/js/index.js
+++ common/js/index.js
@@ -133,3 +133,14 @@ $(function(){
     return false;
   });
 });
+
+// スムーススクロール(faq)
+//ページ内リンクスムーススクロール
+$('a[href^="#"]').on('click', function () {
+  var header = jQuery("header").innerHeight();
+  var href = $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top - header;
+  $("html, body").animate({scrollTop: position}, 550, "swing");
+  return false;
+});
